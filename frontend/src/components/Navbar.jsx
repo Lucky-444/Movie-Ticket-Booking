ebcd574681d7f2407d8f89bf886df10821a7db6e
Lucky <sashankswain6@gmail.com>
2025-10-21 02:18:26 +0530

    added Navbar page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
+      <Link to='/' className="max-md:flex-1">
+        <img src={assets.logo} alt="logo" className="w-48 h-auto" />
+      </Link>
+      <div
+        className="max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
+        max-md:text-lg
+        z-50
+        flex flex-col md:flex-row items-center
+        max-md:justify-center gap-8 md:px-8 py-3 
+        max-md:h-screen
+        md:rounded-full
+        backdrop-blur 
+        bg-black/70 md:bg-white/10
+        md:border border-gray-300/20
+        overflow-hidden
+        transition-[width] duration-300 ease-in-out"
+      >
+        {/* For small screen to hide the small menu icon */}
+        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" />
 
-export default Navbar
+        {/* For Menu Items */}
+        <Link to='/' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Home</Link>
+        <Link to='/movies' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Movies</Link>
+        <Link to='/contact' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Theaters</Link>
+        <Link to='/about' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Releases</Link>
+        <Link to='/favorites' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Favorites</Link>
+      </div>
+
+      <div className="flex items-center gap-8">
+        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
+        <button className="px-4 py-1 sm:px-7 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">Login</button>
+        {/* User login button and search icon */}
+      </div>
+
+      <MenuIcon className="max-md:ml-4 md:hidden cursor-pointer" />
+    </div>
+  );
+};
+
+export default Navbar;
