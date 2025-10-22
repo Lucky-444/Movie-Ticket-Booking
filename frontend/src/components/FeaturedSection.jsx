6e8e676b4f9823a0c0b46918bb5146918d96a11d
Lucky <sashankswain6@gmail.com>
2025-10-23 02:13:18 +0530

    Added Movie Card

--- frontend/src/components/FeaturedSection.jsx
+++ frontend/src/components/FeaturedSection.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
+import { dummyBookingData, dummyShowsData } from "../assets/assets";
+import MovieCard from "./MovieCard";
 
 const FeaturedSection = () => {
   const navigate = useNavigate();
@@ -14,6 +16,24 @@ const FeaturedSection = () => {
         >
           View All{" "}
           <ArrowRight className="group-hover:translate-x-0.5 transition w-5 h-5 ml-2" />
+        </button>
+      </div>
+      <div className="flex flex-wrap max:sm justify-center gap-8 mt-8">
+
+        {dummyShowsData.slice(0, 4).map((show) => (
+                <MovieCard key={show._id} movie={show} />
+        ))}
+      </div>
+      <div className="flex justify-center mt-20">
+        <button
+          onClick={() => {
+            navigate("/movies");
+            scrollTo(0, 0);
+          }}
+          className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition
+                       rounded-md font-medium cursor-pointer"
+        >
+          Show more
         </button>
       </div>
       <div></div>
---
+++ frontend/src/lib/timeFormat.js
@@ -0,0 +1,6 @@
+const timeFormat = (totalMinutes) => {
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+        return `${hours}h ${minutes}m`;
+};
+export default timeFormat;
