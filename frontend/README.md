fa6d1abdd4eb25eb7ae91bd1e85ec574feefdffd
Lucky <sashankswain6@gmail.com>
2025-10-23 01:28:29 +0530

    Added Login and SIgnup Using Clerk account

--- frontend/README.md
+++ frontend/README.md
@@ -1,2 +1,5 @@
 # For TailWind CSS
 refer to : https://tailwindcss.com/docs/installation/using-vite
+
+# For User Authentication Management Used Clerk
+https://clerk.com/
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,46 +1,109 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const {user} = useUser();
+  const {openSignIn} = useClerk();
+  const navigate = useNavigate();
+
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
+      
+      {/* Logo */}
       <Link to='/' className="max-md:flex-1">
         <img src={assets.logo} alt="logo" className="w-48 h-auto" />
       </Link>
+
+      {/* Menu Items */}
       <div
-        className="max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
-        max-md:text-lg
-        z-50
-        flex flex-col md:flex-row items-center
-        max-md:justify-center gap-8 md:px-8 py-3 
-        max-md:h-screen
-        md:rounded-full
-        backdrop-blur 
-        bg-black/70 md:bg-white/10
-        md:border border-gray-300/20
-        overflow-hidden
-        transition-[width] duration-300 ease-in-out"
+        className={`md:flex md:flex-row md:items-center md:px-8 md:rounded-full md:bg-white/10 md:border md:border-gray-300/20 md:backdrop-blur flex flex-col items-center gap-8 py-3
+        max-md:absolute max-md:top-0 max-md:left-0 max-md:h-screen max-md:justify-center max-md:font-medium max-md:text-lg
+        max-md:bg-black/70 max-md:backdrop-blur z-50 overflow-hidden transition-all duration-300 ease-in-out
+        ${isMenuOpen ? "max-md:w-full" : "max-md:w-0"}`}
       >
-        {/* For small screen to hide the small menu icon */}
-        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" />
+        {/* Cross Icon for mobile */}
+        {isMenuOpen && (
+          <XIcon
+            className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer z-50"
+            onClick={() => setIsMenuOpen(false)}
+          />
+        )}
 
-        {/* For Menu Items */}
-        <Link to='/' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Home</Link>
-        <Link to='/movies' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Movies</Link>
-        <Link to='/contact' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Theaters</Link>
-        <Link to='/about' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Releases</Link>
-        <Link to='/favorites' className="mx-4 text-lg font-medium hover:text-primary transition max-md:hidden">Favorites</Link>
+        {/* Menu Links */}
+        <Link 
+          to='/' 
+          onClick={() => { scrollTo(0, 0); setIsMenuOpen(false); }}
+          className="mx-4 font-medium hover:text-primary transition md:mx-4"
+        >
+          Home
+        </Link>
+        <Link 
+          to='/movies' 
+          onClick={() => { scrollTo(0, 0); setIsMenuOpen(false); }}
+          className="mx-4 font-medium hover:text-primary transition md:mx-4"
+        >
+          Movies
+        </Link>
+        <Link 
+          to='/contact' 
+          onClick={() => { scrollTo(0, 0); setIsMenuOpen(false); }}
+          className="mx-4 font-medium hover:text-primary transition md:mx-4"
+        >
+          Theaters
+        </Link>
+        <Link 
+          to='/about' 
+          onClick={() => { scrollTo(0, 0); setIsMenuOpen(false); }}
+          className="mx-4 font-medium hover:text-primary transition md:mx-4"
+        >
+          Releases
+        </Link>
+        <Link 
+          to='/favorites' 
+          onClick={() => { scrollTo(0, 0); setIsMenuOpen(false); }}
+          className="mx-4 font-medium hover:text-primary transition md:mx-4"
+        >
+          Favorites
+        </Link>
       </div>
 
+      {/* Search and Login */}
       <div className="flex items-center gap-8">
-        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-        <button className="px-4 py-1 sm:px-7 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">Login</button>
-        {/* User login button and search icon */}
+        <SearchIcon  className="max-md:hidden w-6 h-6 cursor-pointer" />
+        { 
+          !user ? (
+            <button 
+              onClick={openSignIn} 
+              className="px-4 py-1 sm:px-7 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+            >
+              Login
+            </button>
+          ) : (
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action 
+                  label="My Bookings" 
+                  labelIcon={<TicketPlus width={15} />} 
+                  onClick={() => navigate('/my-bookings')} 
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          )
+        }
+        
       </div>
 
-      <MenuIcon className="max-md:ml-4 md:hidden cursor-pointer" />
+      {/* Hamburger Icon for mobile */}
+      {!isMenuOpen && (
+        <MenuIcon 
+          className="max-md:ml-4 md:hidden cursor-pointer" 
+          onClick={() => setIsMenuOpen(true)} 
+        />
+      )}
     </div>
   );
 };
