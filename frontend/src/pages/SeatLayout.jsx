1d6117190d15f513ea964dc134da269e1df447de
Lucky <sashankswain6@gmail.com>
2025-10-27 02:28:17 +0530

    completed seatLayout Feat

--- frontend/src/pages/SeatLayout.jsx
+++ frontend/src/pages/SeatLayout.jsx
@@ -1,17 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
+import { assets, dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import { ClockIcon } from "lucide-react";
 import isoTimeFormat from "../lib/isoTimeFormat";
+import toast from "react-hot-toast";
 
 const SeatLayout = () => {
+  const groupRows = [
+    ["A", "B"],
+    ["C", "D"],
+    ["E", "F"],
+    ["G", "H"],
+    ["I", "J"],
+  ];
+
   const { id, date } = useParams();
   // const [seats, setSeats] = useState([])
   const [show, setShow] = useState(null);
   const [selectedSeats, setSelectedSeats] = useState([]);
   const [selectedTime, setSelectedTime] = useState(null);
   const navigate = useNavigate();
+
+  const handleSeatClick = (seatId) => {
+    if (!selectedTime) {
+      return toast("Please select a show time first.", { type: "error" });
+    }
+
+    if (!selectedSeats.includes(seatId) && selectedSeats.length > 4) {
+      return toast("You can select a maximum of 5 seats.", { type: "error" });
+    }
+
+    setSelectedSeats((prevSelectedSeats) => {
+      return prevSelectedSeats.includes(seatId)
+        ? prevSelectedSeats.filter((seat) => seat !== seatId)
+        : [...prevSelectedSeats, seatId];
+    });
+  };
+
+  const renderSeats = (row, count = 9) => (
+    <div key={row} className="flex gap-2 mt-2">
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        {Array.from({ length: count }, (_, i) => {
+          const seatId = `${row}${i + 1}`;
+          return (
+            <button
+              key={seatId}
+              onClick={() => handleSeatClick(seatId)}
+              className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${
+                selectedSeats.includes(seatId) && "bg-primary text-white"
+              }`}
+            >
+              {seatId}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 
   const getShow = () => {
     const foundShow = dummyShowsData.find((item) => item._id === id);
@@ -39,8 +85,8 @@ const SeatLayout = () => {
       {/* Available Timings */}
       <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30">
         <p className="text-lg font-semibold px-6">Available Timings</p>
-        {console.log("selectedTime" , selectedTime)}
-        
+        {console.log("selectedTime", selectedTime)}
+
         <div>
           {show.dateTime[date]?.map((item) => (
             <div
@@ -51,7 +97,6 @@ const SeatLayout = () => {
                   ? "bg-primary text-white"
                   : "hover:bg-primary/60"
               }`}
-              
             >
               <ClockIcon className="w-4 h-4" />
               <p className="text-sm">{isoTimeFormat(item.time)}</p>
@@ -60,8 +105,27 @@ const SeatLayout = () => {
         </div>
       </div>
 
-      {/* Seat Layout : */}
-      <div>Seat Layout Here</div>
+      {/* Seat Layout : Main parts */}
+      <div className="relative flex-1 flex flex-col items-center max-md:mt-16">
+        <h1 className="text-2xl font-semibold mb-4">Select your seat</h1>
+        <img src={assets.screenImage} alt="screen" />
+        <p className="text-gray-400 text-sm mb-6">SCREEN SIDE</p>
+
+        <div className="flex flex-col items-center mt-10 text-xs text-gray-400">
+          <div
+            className="grid grid-cols-2 md:grid-cols-1
+            gap-8 md:gap-2 mb-6"
+          >
+            {groupRows[0].map((row) => renderSeats(row))}
+          </div>
+
+          <div className="grid grid-cols-2 gap-11">
+            {groupRows.slice(1).map((group, idx) => (
+              <div key={idx}>{group.map((row) => renderSeats(row))}</div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
