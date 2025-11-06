a785a4b825acae714ffa14cc12a13a470be0cc7a
Lucky <sashankswain6@gmail.com>
2025-11-07 01:47:19 +0530

    creating models for movie and show

---
+++ Backend/models/Show.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const showSchema = new mongoose.Schema(
+         {
+                  movie : {
+                           type : String,
+                           required : true,
+                           ref : "Movie",
+                  },
+                  showDateTime : {
+                           type : Date,
+                           required : true,
+                  },
+                  showPrice : {
+                           type : Number,
+                           required : true,
+                  },
+                  availableSeats : {
+                           type : Object,
+                           default : {},
+                  },
+         },
+         { timestamps : true , minimize : false }
+);
+const Show = mongoose.model("Show", showSchema);
+
+export default Show;
